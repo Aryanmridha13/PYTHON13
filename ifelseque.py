406bb337369146e6cb8332ac226e4aaba06f78d9
Aryan Mridha <aryanmridha8@gmail.com>
2026-08-17 21:55:25 +0530

    if else questions

---
+++ ifelseque.py
@@ -0,0 +1,10 @@
+n = int (input("Enter a number :"))
+if n%3==0:
+    print(f'{n} is divisible by 3 ')
+elif n%5==0:
+    print(f'{n} is divisible by 5 ')
+elif n%7==0:
+    print(f'{n} is divisible by 7 ')    
+else:
+    print(f'{n} is n ot  divisible by 3,5,7 ')
+    
