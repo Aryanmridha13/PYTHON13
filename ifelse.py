c44f9f6b804b4d1a197a55f260f418fe1cbdff72
Aryan Mridha <aryanmridha8@gmail.com>
2026-08-17 21:50:44 +0530

    conidisons

--- ifelse.py
+++ ifelse.py
@@ -1,13 +1,28 @@
-n = int(input("Enter a number :"))
+# n = int(input("Enter a number :"))
 
-if n <= 9 and n >= -9:
-    print(f'{n} is a single digit number')
+# if n <= 9 and n >= -9:
+#     print(f'{n} is a single digit number')
 
-elif (n > 9 and n <= 99) or (n < -9 and n >= -99):
-    print(f'{n} is a double digit number')
+# elif (n > 9 and n <= 99) or (n < -9 and n >= -99):
+#     print(f'{n} is a double digit number')
 
-elif (n > 99 and n <= 999) or (n < -99 and n >= -999):
-    print(f'{n} is a triple digit number')
+# elif (n > 99 and n <= 999) or (n < -99 and n >= -999):
+#     print(f'{n} is a triple digit number')
+
+# else:
+#     print(f'{n} is not single, double or triple digit number')
+
+    # Q2  
+ch = input("Enter a character :")
+
+if ch >= 'A' and ch <= 'Z':
+    print(f'{ch} is an upper case character')
+
+elif ch >= 'a' and ch <= 'z':
+    print(f'{ch} is a lower case character')
+
+elif ch >= '0' and ch <= '9':
+    print(f'{ch} is a numeric character')
 
 else:
-    print(f'{n} is not single, double or triple digit number')
+    print(f'{ch} is a special character')
