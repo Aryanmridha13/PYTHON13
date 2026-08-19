2a10922716f07b295655ebc3d478f603c66e6990
Aryan Mridha <aryanmridha8@gmail.com>
2026-08-19 22:31:59 +0530

    if else questions

---
+++ ifelsequestions/q1.py
@@ -0,0 +1,19 @@
+# n=int(input("enter a num :"))
+# if n >0 :
+#     print("POSITIVE")
+# elif n<0:
+#     print("negitive")
+# elif n==0:
+#     print("ZERO")
+        
+
+n=int(input("Enter a num"))
+n2=int(input("Enter a num"))
+n3=int(input("Enter a num"))
+
+if n>n2 and n>n3:
+    print(f'{n} is grestes ')
+elif n2>n3 and n2>n:
+    print("n2 is greate") 
+else:
+    print("n3 is greate")       
---
+++ ifelsequestions/q2.py
@@ -0,0 +1,9 @@
+age = int(input("Enter  your agr :"))
+if age <= 12  and age>0:
+    print("child")
+elif age >12 and age <=19:
+    print("teen")
+elif age  >=20 and age<=60:
+    print("Adult")
+elif age>60 :
+    print("senor citizen")         
