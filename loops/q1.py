e368d9659e6baf1bd9c2de0cbe707416f9fd5c87
Aryan Mridha <aryanmridha8@gmail.com>
2026-08-21 21:05:30 +0530

    python

---
+++ loops/q1.py
@@ -0,0 +1,10 @@
+# n=int(input("n:"))
+# sum=0
+# for i in range(1,n+1):
+#     sum=i+sum
+#     print(sum)
+
+n=int(input("n:"))
+for i in range(1,n+1):
+    print(i**2,end=" ")
+
---
+++ loops/q2.py
@@ -0,0 +1,15 @@
+# n=int(input("N:"))
+# for i in range(1,n+1):
+#     if i%2==0:
+#         print(i,end=" ")
+# j=int(input("N:"))
+# for i in range(1,j+1):
+#     if i%2!=0:
+#         print(i,end=" ")     
+
+
+for i in range(10,1,-1):
+    print(i,end=" ")
+
+for i in range (10,-10,-2):
+    print(i,end=" ")
