b030942985ab919b72d8849a84c90c0d01b3d9df
Aryan Mridha <aryanmridha8@gmail.com>
2026-08-27 00:03:04 +0530

    questions

--- loops/test4.py
+++ loops/test4.py
@@ -1,3 +1,17 @@
-for i in range(1,1001):
-    if i%7 ==0 and i%5!=0:
-        print(i,end=" ")
+# for i in range(1,1001):
+#     if i%7 ==0 and i%5!=0:
+#         print(i,end=" ")
+
+
+# Q10
+
+# n=int(input("n: "))
+
+# if -10<n<10 :
+#     print("single digit")
+# elif -100<n<100:
+#     print("Double digit")
+
+# else:
+#     print("it is more than single digit or dublr digit")    
+
