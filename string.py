f3282b3bfefbd6b3bd85f83b58b09791c855c853
Aryan Mridha <aryanmridha8@gmail.com>
2026-09-12 01:01:18 +0530

    string in python

---
+++ string.py
@@ -0,0 +1,13 @@
+s='pyspider coaching center'
+print(s.upper())
+print(s.lower())
+print(s.capitalize())
+print(s.title())
+print(s.swapcase())
+
+
+s1= 'teri meri or uski kiski JISki NAHI pAT '
+print(s1.casefold())
+print(s1.count('i'))
+
+
