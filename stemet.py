170e39107bd01d4ce47ebebfc1943f5013cfbb08
Aryan Mridha <aryanmridha8@gmail.com>
2026-09-16 01:30:46 +0530

    string methods

---
+++ stemet.py
@@ -0,0 +1,11 @@
+s='python'
+print(s.upper())
+s1 = "hello python programming"
+print(s1.capitalize())
+print(s1.title())
+print(s1.find('e'))
+print(s1.index('o'))
+print(s1.rindex('o'))
+print(s1.count('m'))
+s2='adminßad'
+print(s2.casefold())
--- string.py
+++ string.py
@@ -11,3 +11,13 @@ print(s1.casefold())
 print(s1.count('i'))
 
 
+print(s1.index('i'))
+print(s1.rindex('i'))
+print(s1.split())
+print(s1.rsplit())
+
+
+s2= 'teri\n meri\n or \n uski  \n kiski \n JISki NAHI pAT '
+print(s2)
+print(s2.splitlines())
+
